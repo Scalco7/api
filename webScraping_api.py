f85cc2865466f182de74afd223b971c6f772f204
Scalco7 <fscalco7@gmail.com>
2024-06-19 16:40:39 -0300

    deploy

--- webScraping_api.py
+++ webScraping_api.py
@@ -1,9 +1,8 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
-from json import dumps
 
 
 def scrapingBianca():
